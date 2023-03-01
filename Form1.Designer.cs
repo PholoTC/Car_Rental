ea1a11caf52ed123b29a2dbe912d7867c6bac772
PholoTC <tebohopholo.tp@gmail.com>
2023-03-01 11:03:08 +0200

    Init

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -36,7 +36,7 @@
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(1564, 1006);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Car Rental";
             this.ResumeLayout(false);
 
         }
